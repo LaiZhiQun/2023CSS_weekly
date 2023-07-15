417e313d52fa481e17213b68a5c02fda48a62ac9
LaiZhiQun <wcjh0911@gmail.com>
2023-07-15 23:23:58 +0800

    完成首頁a連結

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,20 @@
 <template lang="pug">
-p home 
+.home
+  RouterLink(to='/week2') week2 作業
 </template>
+
+<style>
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.home a {
+  padding: 1rem;
+  text-decoration: none;
+  font-size: 25px;
+}
+.home a:hover {
+  color: rgb(109, 216, 109);
+}
+</style>
