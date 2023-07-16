3dcf7d6baaabb28c463b97cf629be601ef960210
LaiZhiQun <wcjh0911@gmail.com>
2023-07-16 11:05:32 +0800

    完成nav

--- src/components/week2/navComp.vue
+++ src/components/week2/navComp.vue
@@ -1,3 +1,27 @@
-<template lang="pug">
-p nav
+<template lang="pug"> 
+nav
+  ul.d-flex.container.aic
+    li 
+      RouterLink(to="#") WILLIAM LEE
+    li 
+      RouterLink(to="#") BASED IN TAIWAN
+    li 
+      RouterLink(to="#") CURRENTLY FRONT END ENGINEER AT HEXSCHOOL
+    li 
+      RouterLink(to="#") CONTACT ME
 </template>
+
+<style lang="scss" scoped>
+* {
+  background-color: black;
+  color: white;
+}
+a:hover {
+  color: #D1D1D1
+}
+ul {
+  justify-content: space-between;
+  padding-bottom: 24px;
+  padding-top: 40px;
+}
+</style>
