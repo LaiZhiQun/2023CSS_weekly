28b5f813bc36575357149c08adfd376ce29a77c1
LaiZhiQun <wcjh0911@gmail.com>
2023-07-16 17:05:41 +0800

    完成 contact

--- src/components/week2/contactComp.vue
+++ src/components/week2/contactComp.vue
@@ -1,3 +1,31 @@
 <template lang="pug">
-p contact
+.contact 
+  .container.d-flex.jcsb.aic 
+    .text.d-flex
+      p CONTACT ME
+      img(src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/2023week2/trending_flat_black_24dp%201.png" alt="arrow")
+    .icon
+      img(src="https://github.com/hexschool/2022-web-layout-training/blob/main/2023week2/discord.png?raw=true" alt="discord")
+      img.mx-20(src="https://github.com/hexschool/2022-web-layout-training/blob/main/2023week2/instagram.png?raw=true" alt="instagram")
+      img(src="https://github.com/hexschool/2022-web-layout-training/blob/main/2023week2/message.png?raw=true" alt="message")
 </template>
+
+<style scoped>
+* {
+  background: linear-gradient(#050014, #01021E);
+}
+.contact {
+  padding: 96px 0;
+}
+.text p {
+  color: white;
+  font-size: 64px;
+}
+.mx-20 {
+  margin: 0 20px;
+}
+.icon img {
+  width: 164px;
+  height: 164px;
+}
+</style>
