ccf3b5260ea4eba4ed089f5c78dda4e4f88c9644
LaiZhiQun <wcjh0911@gmail.com>
2023-07-16 14:46:01 +0800

    更新introduction樣式

--- src/components/week2/introductionComp.vue
+++ src/components/week2/introductionComp.vue
@@ -1,3 +1,41 @@
 <template lang="pug">
-p intro  
+.introduction 
+    .container.d-flex.jcsb
+      .section-left.w-526
+        p Introduction
+        p Hi, I'm William Lee, a Front-End Developer with a passion for building beautiful and responsive websites that provide an optimal user experience.
+        div.mt-16 
+          img(src="https://github.com/hexschool/2022-web-layout-training/blob/main/2023week2/sparkle-sm.png?raw=true" alt="sparkle-sm")
+          img(src="https://github.com/hexschool/2022-web-layout-training/blob/main/2023week2/sparkle-sm.png?raw=true" alt="sparkle-sm")
+          img(src="https://github.com/hexschool/2022-web-layout-training/blob/main/2023week2/sparkle-sm.png?raw=true" alt="sparkle-sm")
+      .section-right.d-flex.jcsb
+        img.me-24(src="https://github.com/hexschool/2022-web-layout-training/blob/main/2023week2/sparkle.png?raw=true" alt="sparkle")
+        img.me-24(src="https://github.com/hexschool/2022-web-layout-training/blob/main/2023week2/photographer.png?raw=true" alt="photographer")
+        img(src="https://github.com/hexschool/2022-web-layout-training/blob/main/2023week2/people-2.png?raw=true" alt="people-2")
 </template>
+  
+  <style scoped>
+  * {
+    background-color: black;
+    color: white;
+  }
+  .introduction {
+    padding: 160px 0;
+  }
+  .mt-16 {
+    margin-top: 16px;
+  }
+  .section-left p:nth-child(1) {
+    font-size: 64px;
+  }
+  .section-left p {
+    font-size: 24px;
+    padding: 16px 0;
+  }
+  .w-526 {
+    width: 526px;
+  }
+  .me-24 {
+    margin-right: 24px;
+  }
+  </style>
