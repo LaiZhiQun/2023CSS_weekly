54aed9ce1a09b4ddbe28adbdcbf750c11219d30e
LaiZhiQun <wcjh0911@gmail.com>
2023-07-16 18:37:51 +0800

    完成 footer

--- src/components/week2/footerComp.vue
+++ src/components/week2/footerComp.vue
@@ -1,3 +1,19 @@
 <template lang="pug">
-p footer  
+.footer 
+  .container.text-center 
+    p.py-40 all rights reserved @hexschool2023
 </template>
+
+<style scoped>
+.py-40 {
+  padding: 40px 0;
+}
+.footer {
+  background-color: black;
+  color: white;
+}
+.text-center {
+  text-align: center;
+}
+
+</style>
