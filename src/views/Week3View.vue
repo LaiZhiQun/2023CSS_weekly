53eb87560ed21e894d21c3b9033339a0a66b8747
LaiZhiQun <wcjh0911@gmail.com>
2023-07-23 17:32:19 +0800

    增加 week共用樣式

--- src/views/Week3View.vue
+++ src/views/Week3View.vue
@@ -1,3 +1,53 @@
 <template lang="pug">
-p week3
+
+Nav
+Footer
 </template>
+
+<script setup>
+// eslint-disable-next-line no-unused-vars
+import Nav from '@/components/week3/navComp.vue'
+// eslint-disable-next-line no-unused-vars
+import Footer from '@/components/week3/footerComp.vue'
+</script>
+
+<style>
+.container {
+  max-width: 1296px;
+  margin: 0 auto;
+}
+
+.d-flex {
+  display: flex;
+}
+/* justify-content */
+.jcc {
+  justify-content: center;
+}
+.jcs {
+  justify-content: start;
+}
+.jce {
+  justify-content: end;
+}
+.jcsb {
+  justify-content: space-between;
+}
+.jcsa {
+  justify-content: space-around;
+}
+.jcse {
+  justify-content: space-evenly;
+}
+/* align-items */
+.aic {
+  align-items: center;
+}
+.ais {
+  align-items: start;
+}
+.aie {
+  align-items: end;
+}
+
+</style>
